ca5712573c4a25bc1bbf2739e0e3db4d342e4c47
Amark19 <amarkhamkar6@gmail.com>
2023-11-09 00:45:59 +0530

    Image autoscaling by scale factor

--- src/utils/menusManager/addImage.js
+++ src/utils/menusManager/addImage.js
@@ -1,5 +1,6 @@
 import { fabric } from 'fabric';
 
+const AUTO_SCALE = 0.25;
 export const addImage = (canvas) => {
     const input = document.createElement('input');
     input.type = 'file';
@@ -12,11 +13,20 @@ export const addImage = (canvas) => {
             const data = f.target.result;
             fabric.Image.fromURL(data, (img) => {
                 //scale down image
-                const oImg = img.set({ left: 0, top: 0, angle: 0 }).scale(0.5);
+                const oImg = img.set({ left: 0, top: 0 , angle: 0 }).scale(autoScaleByImageSize(canvas, img));
                 canvas.add(oImg);
                 sessionStorage.setItem('canvas', JSON.stringify(canvas.toJSON()));
             });
         };
     };
     input.click();
+}
+
+function autoScaleByImageSize(canvas, img) {
+    const { width, height } = img;
+    const canvasWidth = canvas.width;
+    const canvasHeight = canvas.height;
+    const scaleX = canvasWidth / width;
+    const scaleY = canvasHeight / height;
+    return Math.min(scaleX*AUTO_SCALE, scaleY*AUTO_SCALE);
 }
