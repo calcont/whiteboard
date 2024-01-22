631c7501e7bd6138dfbd6e0008cc1b89dd2d3b7d
Amark19 <amarkhamkar6@gmail.com>
2024-01-22 23:18:45 +0530

    Zoom ui fixed

--- src/components/CanvasEditor/Footer/Footer.jsx
+++ src/components/CanvasEditor/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import "./Footer.css";
 
 const Footer = () => {
     return (
-        <footer className="footer">
+        <div className="footer">
             <ZoomPanel/>
-        </footer>
+        </div>
     )
 }
 
