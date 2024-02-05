1bdcb5bb5067e01d722b8832f393fa2175bb4c26
Amark19 <amarkhamkar6@gmail.com>
2024-02-05 23:56:27 +0530

    changed zoom scale to 25%

--- src/utils/Zoom.js
+++ src/utils/Zoom.js
@@ -1,7 +1,7 @@
 export const handleZoomUtil = (sign, canvas) => {
     if (canvas) {
         let zoom = canvas.getZoom();
-        zoom += 0.1 * sign;
+        zoom += 0.25 * sign;
         if (zoom > 20) zoom = 20;
         if (zoom < 0.01) zoom = 0.01;
         return zoom;
