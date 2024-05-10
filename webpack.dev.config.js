a961fac9fe14618db23436551d2fac26991b2e97
Amark19 <amarkhamkar6@gmail.com>
2024-05-11 02:16:26 +0530

    Configured webpack

---
+++ webpack.dev.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    mode: "development",
+    devtool: 'inline-source-map',
+    plugins: [new HtmlWebpackPlugin(
+        {
+            template: path.resolve(__dirname, "public", "index.html"),
+            favicon: "./public/favicon.ico",
+            filename: "index.html",
+            manifest: "./public/manifest.json",
+        }
+    )],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", ["@babel/preset-react", {runtime: "automatic"}]],
+                    },
+                },
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                exclude: /node_modules/,
+                use: [
+                    "style-loader",
+                    {loader: "css-loader"},
+                    {
+                        loader: "postcss-loader",
+                    },
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                type: 'asset/resource'
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+    devServer: {
+        static: path.join(__dirname, "public"), // Serve files from this directory
+        port: 3000, // Port for the development server
+        open: true, // Open the default web browser when the server starts
+    },
+};
---
+++ webpack.prod.config.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    mode: "production",
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
+    plugins: [new HtmlWebpackPlugin(
+        {
+            template: path.resolve(__dirname, "public", "index.html"),
+            favicon: "./public/favicon.ico",
+            filename: "index.html",
+            manifest: "./public/manifest.json",
+        }
+    )],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", ["@babel/preset-react", {runtime: "automatic"}]],
+                    },
+                },
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                exclude: /node_modules/,
+                use: [
+                    "style-loader",
+                    {loader: "css-loader"},
+                    {
+                        loader: "postcss-loader",
+                    },
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                type: 'asset/resource'
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+    devServer: {
+        static: path.join(__dirname, "public"), // Serve files from this directory
+        port: 3000, // Port for the development server
+        open: true, // Open the default web browser when the server starts
+    },
+};
