133ce0b5d31f8ce552e0336aa8d5a23c8d1c70cf
Amar Khamkar <71604396+Amark19@users.noreply.github.com>
2024-05-11 17:03:53 +0530

    Configured webpack (#25)
    * Configured webpack
    
    * formatted webpack files
    
    * added sentry-webpack to upload sourcemaps
    
    * added support to for process.env in webpack
    
    * removed devserver from prod webpack

---
+++ webpack.prod.config.js
@@ -0,0 +1,68 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { sentryWebpackPlugin } = require("@sentry/webpack-plugin");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "production",
+  devtool: "source-map",
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+      favicon: "./public/favicon.ico",
+      filename: "index.html",
+      manifest: "./public/manifest.json",
+    }),
+    sentryWebpackPlugin({
+      org: "calcont",
+      project: "javascript-react",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(dotenv.config().parsed),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              ["@babel/preset-react", { runtime: "automatic" }],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          { loader: "css-loader" },
+          {
+            loader: "postcss-loader",
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
